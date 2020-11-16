51f95f0f741814ddafcc5257064efe839abb4b0c
erik torkelsson <erik.torkelsson@hotmail.com>
2020-11-16 16:32:22 +0100

    asdf

---
+++ CarRental/Models/ViewModels/CreateRentalViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Models.ViewModels
+{
+    public class CreateRentalViewModel
+    {
+        public Rental Rental { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
